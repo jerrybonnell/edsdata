5dc05479d274bfc0037548d5979673c0892b19e8
Jerry Bonnell <j.bonnell@miami.edu>
2022-12-13 23:57:47 -0500

    alana

--- R/alana.R
+++ R/alana.R
@@ -4,6 +4,7 @@
 #'
 #' @format A tibble with 80 rows and 3 columns
 #' \describe{
+#'   \item{day}{the day number}
 #'   \item{heron}{whether a heron was observed (`1`) or not (`0`)}
 #'   \item{fish}{whether a fish was observed (`1`) or not (`0`)}
 #'   \item{squirrel}{whether a squirrel was observed (`1`) or not (`0`)}
--- data-raw/alana/alana.R
+++ data-raw/alana/alana.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
-alana <- read_csv("data-raw/alana/alana.csv")
+alana <- read_csv("data-raw/alana/alana.csv") %>%
+  mutate(day = seq(1, 80)) %>%
+  relocate(day, .before = heron)
 
 usethis::use_data(alana, overwrite = TRUE)
