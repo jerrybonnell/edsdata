107cbf36d7d24255cc6aff6af898e8313f4d0fb4
Jerry Bonnell <j.bonnell@miami.edu>
2022-12-25 19:06:05 -0500

    scatter with R

---
+++ R/scatter_with_r.R
@@ -0,0 +1,14 @@
+
+scatter_with_r <- function(r) {
+  x <- rnorm(10000)
+  z <- rnorm(10000)
+  y = r*x + (sqrt(1-r**2))*z
+  g <- tibble(x = x, y = y) %>%
+    ggplot() +
+    geom_point(aes(x = x, y = y), color = "darkcyan", alpha = 0.8) +
+    xlab("") +
+    ylab("")
+  return(g)
+}
+
+usethis::use_r("scatter_with_r")
